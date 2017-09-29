6995bbcd4e405ce32210594802f2e6f025eb7654
Katherine Ebel <kathyebel@me.com>
2017-09-29 17:25:58 -0400

    fix error with formControlName directives in profile form

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -7,26 +7,26 @@ import {Router} from '@angular/router';
   templateUrl: 'app/user/profile.component.html'
 })
 export class ProfileComponent implements OnInit {
-  private profileForm: FormGroup;
+  profileForm: FormGroup;
 
   constructor(private _auth: AuthService,
               private _router: Router) {}
   ngOnInit(): void {
-    // TODO: bug not working -cannot read property 'firstName' of undefined
     const firstName = new FormControl(this._auth.currentUser.firstName);
     const lastName = new FormControl(this._auth.currentUser.lastName);
 
     this.profileForm = new FormGroup({
       lastName: lastName,
       firstName: firstName
-    })
+    });
+    console.log(this.profileForm)
+      console.log(firstName)
   }
 
   saveProfile(formValues) {
     this._auth.updateCurrentUser(formValues.firstName, formValues.lastName);
-    // this._router.navigate(['events'])
   }
   cancel() {
-    // this._router.navigate(['events'])
+    this._router.navigate(['events'])
   }
 }
