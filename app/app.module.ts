52215f9704ce94946a3d9f806f23f7707c50b8a5
Katherine Ebel <kathyebel@me.com>
2017-09-30 12:24:30 -0400

    content-projection: add collapsible component for session details

--- app/app.module.ts
+++ app/app.module.ts
@@ -13,6 +13,7 @@ import {
 import {EventsAppComponent} from './events-app.component';
 import {NavBarComponent} from './nav/navbar.component';
 import {ToastrService} from './common/toastr.service';
+import { CollapsibleWellComponent } from "./common/collapsible-well.component";
 import {appRoutes} from './routes';
 import {Error404Component} from './errors/404.component';
 import {AuthService} from './user/auth.service';
@@ -42,7 +43,8 @@ const checkDirtyState = (component: CreateEventComponent) => component.isDirty ?
       CreateEventComponent,
       Error404Component,
       CreateSessionComponent,
-      SessionListComponent
+      SessionListComponent,
+      CollapsibleWellComponent
     ],
     providers: [
       EventService,
